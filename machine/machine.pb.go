db8cf0bcfe42a887a05bae0e8e1cdef3721c9dd7
AlohaFest <87898681+AlohaFest@users.noreply.github.com>
2022-09-14 11:04:16 +0000

    more cleaning up

--- machine/machine.pb.go
+++ machine/machine.pb.go
@@ -30,3 +30,4 @@ type Instruction struct {
 	Operand              int32    `protobuf:"varint,2,opt,name=operand,proto3" json:"operand,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
