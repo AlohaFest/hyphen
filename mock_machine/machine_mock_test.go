a8115f9a61fd6e559f0fed76ea5f4e494ec3a6fa
AlohaFest <87898681+AlohaFest@users.noreply.github.com>
2022-10-18 12:46:00 +0000

    GESTCOM V1.0.0

--- mock_machine/machine_mock_test.go
+++ mock_machine/machine_mock_test.go
@@ -4,3 +4,14 @@
 // Copyright (C) 2020 Toran Sahu <toran.sahu@yahoo.com>
 //
 // Distributed under terms of the MIT license.
+//
+
+package mock_machine_test
+
+import (
+	context "context"
+	"log"
+	"testing"
+	"time"
+
+	gomock "github.com/golang/mock/gomock"
