d3b63d82bde234426e63688b1c80ad5ec221ae70
AlohaFest <87898681+AlohaFest@users.noreply.github.com>
2022-11-20 12:37:14 +0000

    commit all

--- server/machine_live_test.go
+++ server/machine_live_test.go
@@ -32,3 +32,8 @@ func init() {
 	go func() {
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+}
+
+func bufDialer(context.Context, string) (net.Conn, error) {
